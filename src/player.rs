8633ee8dd0bf2122b1a0600f8191a04a0a8925e6
Matt Lander <matthew.lander@emperor.works>
2026-08-25 14:06:06 +0100

    tweaked sizes of things

--- src/player.rs
+++ src/player.rs
@@ -14,7 +14,7 @@ use crate::{ animation_manager::AnimationManager, assets::AssetLoadState, game_g
 
 const PLAYER_SPEED: f32 = 10.;
 const PLAYER_TURN_SPEED: f32 = 3.0;
-const PLAYER_COLLISION_RADIUS:f32 = 0.4;
+const PLAYER_COLLISION_RADIUS:f32 = 0.5;
 const PLAYER_RESTITUTION:f32 = 0.6;
 pub const PLAYER_HEIGHT:f32 = 1.8;
 pub const PLAYER_MAX_DRIBBLE_DISTANCE:f32 = 1.4;
